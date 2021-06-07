c1ebc21389e1570794d92a3c4ded0571bd2d745d
Maxim Samsonov <m.samsonov@computer.org>
2021-06-07 17:57:21 +0300

    Update ReedSolomonTest_D16.java

--- src/test/java/libcorrect/reed_solomon/ReedSolomonTest_D16.java
+++ src/test/java/libcorrect/reed_solomon/ReedSolomonTest_D16.java
@@ -5,12 +5,9 @@ import java.util.Random;
 import static libcorrect.reed_solomon.CorrectReedSolomon.correctRsPrimitivePolynomialCcsds;
 
 public class ReedSolomonTest_D16 extends ReedSolomonTest {
-    private final static Random RANDOM = new Random(1);
-    private final long blockLength_U = 255;
-    private final long minDistance_U = 32;
-    private final long messageLength_U = blockLength_U - minDistance_U;
-    private CorrectReedSolomon rs = new CorrectReedSolomon(correctRsPrimitivePolynomialCcsds, (byte)1, (byte)1, minDistance_U);
-    private RSTestbench testbench = new RSTestbench(blockLength_U, minDistance_U);
+    void ReadSolominTest_32() {
+        init(16);
+    }
     
     @Test
     void RS_T_1() {
