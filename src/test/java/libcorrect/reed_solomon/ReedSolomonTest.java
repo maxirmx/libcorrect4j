d4d3d710c748356e5a7492e1de87cf73728c9dea
Maxim Samsonov <m.samsonov@computer.org>
2021-06-07 17:55:29 +0300

    Update ReedSolomonTest.java

--- src/test/java/libcorrect/reed_solomon/ReedSolomonTest.java
+++ src/test/java/libcorrect/reed_solomon/ReedSolomonTest.java
@@ -3,10 +3,22 @@ package libcorrect.reed_solomon;
 import java.util.Random;
 
 public class ReedSolomonTest {
-    protected final static Random RANDOM = new Random(1);
+    protected final Random RANDOM = new Random(1);
     
-    protected static void runTests(CorrectReedSolomon rs, RSTestbench testbench, long blockLength_U,
-                                long testMsgLength_U, long numErrors_U, long numErasures_U, long numIterations_U) {
+    protected final long blockLength_U = 255;
+    protected long minDistance_U;
+    protected long messageLength_U;
+    protected CorrectReedSolomon rs;
+    protected RSTestbench testbench;
+    
+    protected void init(long mDistance) {
+        minDistance_U = mDistance;
+        messageLength_U = blockLength_U - minDistance_U;
+        rs = new CorrectReedSolomon(correctRsPrimitivePolynomialCcsds, (byte)1, (byte)1, minDistance_U);
+        testbench = new RSTestbench(blockLength_U, minDistance_U);
+    }
+  
+    protected void runTests(long testMsgLength_U, long numErrors_U, long numErasures_U, long numIterations_U) {
         System.out.printf("testing reed solomon block length=%d, message length=%d, errors=%d, erasures=%d...", blockLength_U, testMsgLength_U, numErrors_U, numErasures_U);
         for (long i_U = 0; Long.compareUnsigned(i_U, numIterations_U) < 0; i_U++) {
             RsTestRun run = testbench.testRsErrors(rs, testMsgLength_U, numErrors_U, numErasures_U);
